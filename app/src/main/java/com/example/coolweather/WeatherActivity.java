f2bec979f62047565ca92c8d8ed01eb7770ee51f
zhaowenhao1234 <199907qQ>
2018-10-07 14:04:20 +0800

    #根据最新和风天气API接口作出修改（接口原因，aqi信息有无法访问的情况

--- app/src/main/java/com/example/coolweather/WeatherActivity.java
+++ app/src/main/java/com/example/coolweather/WeatherActivity.java
@@ -1,13 +1,19 @@
 package com.example.coolweather;
 
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.ScrollView;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.example.coolweather.db.MydatabaseHelper;
 import com.example.coolweather.gson.Forecast;
 import com.example.coolweather.gson.Lifestyle;
 import com.example.coolweather.gson.Weather;
@@ -44,13 +50,20 @@ public class WeatherActivity extends AppCompatActivity {
 
     private String mWeatherId;
 
-    private List<String> url=null;
+    private List<String> url = null;
+    private ContentValues contentValues = null;
+
+    private MydatabaseHelper dbHelper;
+    private SQLiteDatabase db;
+    private boolean flag;
+    private int num=0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_weather);
+        dbHelper = new MydatabaseHelper(this, "WeatherInfo.db", null, 1);
+        db = dbHelper.getReadableDatabase();
         // 初始化各控件
         weatherLayout = (ScrollView) findViewById(R.id.weather_layout);
         titleCity = (TextView) findViewById(R.id.title_city);
@@ -61,10 +74,49 @@ public class WeatherActivity extends AppCompatActivity {
         suggestionLayout = (LinearLayout) findViewById(R.id.suggestion_layout);
         aqiText = (TextView) findViewById(R.id.aqi_txt);
         pm25Text = (TextView) findViewById(R.id.pm25_txt);
-        // 无缓存时去服务器查询天气
         mWeatherId = getIntent().getStringExtra("weather_id");
-        weatherLayout.setVisibility(View.INVISIBLE);
-        requestWeather(mWeatherId);
+        //先查询本地数据库
+        flag = requestFromDb(mWeatherId);
+        if (flag == false) {
+            // 无缓存时去服务器查询天气
+            weatherLayout.setVisibility(View.INVISIBLE);
+            requestWeather(mWeatherId);
+        }
+    }
+
+    /**
+     * 从数据库中查找天气信息
+     */
+    public boolean requestFromDb(String weatherId) {
+        Cursor cursor = db.query("weather", null,
+                "weatherid = ?", new String[]{weatherId}, null, null, null);
+        if ((cursor.getCount()) >0) {
+            cursor.moveToFirst();
+            List<String> infoList = new ArrayList<>();
+            String now = cursor.getString(cursor.getColumnIndex("now"));
+            infoList.add(now);
+            String aqi = cursor.getString(cursor.getColumnIndex("aqi"));
+            infoList.add(aqi);
+            String forecast = cursor.getString(cursor.getColumnIndex("forecast"));
+            infoList.add(forecast);
+            String lifestyle = cursor.getString(cursor.getColumnIndex("lifestyle"));
+            infoList.add(lifestyle);
+            cursor.close();
+            for (int i = 0; i < 4; i++) {
+                final Weather weather = Utility.handleWeatherResponse(infoList.get(i));
+                final int finalI = i;
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        showWeatherInfo(weather, finalI);
+                    }
+                });
+            }
+            infoList.clear();
+            return true;
+        } else {
+            return false;
+        }
     }
 
 
@@ -72,11 +124,11 @@ public class WeatherActivity extends AppCompatActivity {
      * 根据天气id请求城市天气信息。
      */
     public void requestWeather(final String weatherId) {
-
+        contentValues = new ContentValues();
         url = new ArrayList<>();
-        String weatherUrl = "https://free-api.heweather.com/s6/weather/now?location="
+        String nowUrl = "https://free-api.heweather.com/s6/weather/now?location="
                 + weatherId + "&key=06761d21d36044c0946c0061794423f6";
-        url.add(weatherUrl);
+        url.add(nowUrl);
         String aqiUrl = "https://free-api.heweather.com/s6/air/now?location="
                 + weatherId + "&key=06761d21d36044c0946c0061794423f6";
         url.add(aqiUrl);
@@ -97,11 +149,38 @@ public class WeatherActivity extends AppCompatActivity {
                     runOnUiThread(new Runnable() {
                         @Override
                         public void run() {
-                            if (weather != null && "ok".equals(weather.status)) {
+                            if (weather != null) {
                                 showWeatherInfo(weather, finalI);
+                                num++;
+                                //将信息存入数据库
+                                switch (finalI) {
+                                    case 0:
+                                        contentValues.put("weatherid", weatherId);
+                                        contentValues.put("now", responseText);
+                                        break;
+                                    case 1:
+                                        if(weather.status.equals("ok")){
+                                            contentValues.put("aqi", responseText);
+                                        }else if(weather.status.equals("permission denied")){
+                                            contentValues.put("aqi", "暂无数据");
+                                        }
+                                        break;
+                                    case 2:
+                                        contentValues.put("forecast", responseText);
+                                        break;
+                                    case 3:
+                                        contentValues.put("lifestyle", responseText);
+                                        break;
+                                }
+                                //当所有的
+                                if(num==4){
+                                    db.insert("weather", null, contentValues);
+                                    contentValues.clear();
+                                    url.clear();
+                                    num=0;
+                                }
                             } else {
-                                Toast.makeText(WeatherActivity.this, "获取天气信息失败", Toast.LENGTH_SHORT).show();
-                                showWeatherInfo(null,1);
+                                Toast.makeText(WeatherActivity.this, "获取天气信息失败123", Toast.LENGTH_SHORT).show();
                             }
                         }
                     });
@@ -119,6 +198,7 @@ public class WeatherActivity extends AppCompatActivity {
                 }
             });
         }
+
     }
 
     /**
@@ -129,8 +209,8 @@ public class WeatherActivity extends AppCompatActivity {
         switch (num) {
             case 0:
                 String cityName = weather.basic.cityName;
-                String updateTime=weather.upate.updateTime.split(" ")[1];
-                String degree = weather.now.temprature + "℃";
+                String updateTime = weather.upate.updateTime.split(" ")[1];
+                String degree = weather.now.temp + "℃";
                 String weatherInfo = weather.now.info;
                 titleCity.setText(cityName);
                 titleUpdateTime.setText(updateTime);
@@ -138,15 +218,16 @@ public class WeatherActivity extends AppCompatActivity {
                 weatherInfoText.setText(weatherInfo);
                 break;
             case 1:
-                if(weather!=null){
+                if (weather!=null&&!weather.status.equals("permission denied")) {
                     aqiText.setText(weather.aqi.aqi);
                     pm25Text.setText(weather.aqi.pm25);
-                }else{
+                } else  {
                     aqiText.setText("暂无数据");
                     pm25Text.setText("暂无数据");
                 }
                 break;
             case 2:
+                forecastLayout.removeAllViews();
                 for (Forecast forecast : weather.forecastList) {
                     View view = LayoutInflater.from(this).inflate(R.layout.forecast_item, forecastLayout, false);
                     TextView dateText = (TextView) view.findViewById(R.id.date_txt);
@@ -170,7 +251,6 @@ public class WeatherActivity extends AppCompatActivity {
                     suggestionLayout.addView(view);
                 }
                 weatherLayout.setVisibility(View.VISIBLE);
-                url.clear();
                 break;
         }
     }
--- app/src/main/java/com/example/coolweather/gson/Now.java
+++ app/src/main/java/com/example/coolweather/gson/Now.java
@@ -4,7 +4,7 @@ import com.google.gson.annotations.SerializedName;
 
 public class Now {
     @SerializedName("tmp")
-    public String temprature;
+    public String temp;
 
     @SerializedName("cond_txt")
     public String info;
